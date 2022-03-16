523c2259a4fcf1c3863180e6ab8230051a7a2ab3
songkangfeng <songkangfeng@gamil.com>
2022-03-16 20:23:06 +0800

    update

--- w3/contracts/Vault.sol
+++ w3/contracts/Vault.sol
@@ -1,9 +1,28 @@
 pragma solidity ^0.8.0; 
 
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
 contract Vault {
 
-receive(){
-    
+address public tokenAddr;
+mapping(address => uint) public balances;
+
+//保存相关合约地址
+constructor(address _tokenAddr) {
+    tokenAddr = _tokenAddr;
 }
+//存款
+function despote(uint _amount) public{
+    require(IERC20(tokenAddr).transferFrom(msg.sender, address(this), _amount));
+    balances[msg.sender] += _amount;
+}
+
+//取款
+function withdraw(uint _amount) public {
+require(balances[msg.sender] >= _amount && _amount > 0);
+        balances[msg.sender] -= _amount;
+require(IERC20(tokenAddr).transfer(msg.sender, _amount));
+}
+
 
 }
